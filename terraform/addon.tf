fb8ef3e888ed1cba3b4f932192c611ab20be2c36
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 15:42:48 -0400

    addon

--- terraform/addon.tf
+++ terraform/addon.tf
@@ -4,6 +4,7 @@ resource "helm_release" "nginx_ingress" {
   chart      = "ingress-nginx"
   namespace  = "ingress-nginx"
   create_namespace = true
+  
   set {
     name  = "controller.service.type"
     value = "LoadBalancer"

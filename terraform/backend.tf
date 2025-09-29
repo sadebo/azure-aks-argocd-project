14ffb1018b07dd7834efba6324541e45ec84d83b
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 13:58:53 -0400

    tfstflask1234 - storage

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "tfstate-rg"
-    storage_account_name  = "tfstateaksdemo"   # replace with your actual storage account name
+    storage_account_name  = "tfstflask1234"   # replace with your actual storage account name
     container_name        = "tfstate"
     key                   = "aks.tfstate"
   }

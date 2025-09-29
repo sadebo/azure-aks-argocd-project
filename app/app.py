cff6575b0339651b86e74f876a8548b72b36ae83
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 19:18:26 -0400

    updates dockerfile

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
-from flask import Flask
+import os
+import redis
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
+# Redis connection
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", "6379"))
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 @app.route("/")
 def index():
-    return "Hello from Flask running on AKS with ArgoCD 🚀"
+    # Increment a counter in Redis
+    visits = r.incr("page_visits")
+    return render_template("index.html", visits=visits)
 
 @app.route("/healthz")
 def healthz():
-    return {"status": "ok"}, 200
+    return "ok", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

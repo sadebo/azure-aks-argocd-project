f1636785505a4a257c00ecfc076ce67422d7d21e
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 13:27:30 -0400

    update storage name

--- storage/main.tf
+++ storage/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "tfstate_rg" {
 }
 
 resource "azurerm_storage_account" "tfstate_sa" {
-  name                     = "tfstateaksdemo"   # must be globally unique, lowercase, <=24 chars
+  name                     = "tfstateaksdemo-dev"   # must be globally unique, lowercase, <=24 chars
   resource_group_name      = azurerm_resource_group.tfstate_rg.name
   location                 = azurerm_resource_group.tfstate_rg.location
   account_tier             = "Standard"
@@ -17,6 +17,6 @@ resource "azurerm_storage_account" "tfstate_sa" {
 
 resource "azurerm_storage_container" "tfstate_container" {
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate_sa.name
+  storage_account_id    = azurerm_storage_account.tfstate_sa.id
   container_access_type = "private"
 }
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaksdemo"
+    storage_account_name = "tfstateaksdemo-dev"
     container_name       = "tfstate"
     key                  = "aks.tfstate"
   }

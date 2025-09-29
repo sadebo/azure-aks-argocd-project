16d3fe09386a8d684f2db1b0865acb97af2003aa
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 14:41:20 -0400

    addon

--- terraform/addon.tf
+++ terraform/addon.tf
@@ -3,7 +3,6 @@ resource "helm_release" "nginx_ingress" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   namespace  = "ingress-nginx"
-
   create_namespace = true
   set {
     name  = "controller.service.type"

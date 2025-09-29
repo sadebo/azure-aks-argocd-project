e52cc76811cc76e2b75ba035e6309d052c7ef1de
Sanu Adebo <oluwasanu.e.adebo.ctr@mail.com>
2025-09-29 13:30:10 -0400

    storage name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateaksdemo-dev"
+    storage_account_name = "tfstateaksdemo-dev-1234"
     container_name       = "tfstate"
     key                  = "aks.tfstate"
   }
